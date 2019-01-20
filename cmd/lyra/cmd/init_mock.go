f2e06912a3585e6cfa6d0e7decf48775881ee815
jdwelch <me@jdwelch.net>
2019-01-20 12:59:50 +0000

    scaffold of init + flags
    Signed-off-by: jdwelch <me@jdwelch.net>
    
    bump mods
    
    Signed-off-by: jdwelch <me@jdwelch.net>

--- cmd/lyra/cmd/init_mock.go
+++ cmd/lyra/cmd/init_mock.go
@@ -2,9 +2,23 @@ package cmd
 
 import (
 	"fmt"
+	"path/filepath"
+
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
 
 	t "github.com/lyraproj/lyra/pkg/strings"
+
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var (
+	connectiontimeout int
+	host              string
+	kubeconfig        string
+	kubecontext       string
+	kubenamespace     string
 )
 
 // NewInitCmd returns the init subcommand
@@ -19,14 +33,45 @@ func NewInitCmd() *cobra.Command {
 		SuggestFor: []string{"install"},
 	}
 
+	// Flags for k8s
+	if home := homedir.HomeDir(); home != "" { // Not sure why I have to do this, but if I don't kube client complains later.
+		cmd.PersistentFlags().StringVar(&kubeconfig, "kubeconfig", filepath.Join(home, ".kube", "config"), "absolute path to the kubeconfig file to use")
+	} else {
+		cmd.PersistentFlags().StringVar(&kubeconfig, "kubeconfig", "", "absolute path to the kubeconfig file to use")
+	}
+	cmd.PersistentFlags().IntVarP(&connectiontimeout, "cygnus-connection-timeout", "t", 300, "the duration (in seconds) Lyra will wait to establish a connection to Cygnus")
+	cmd.PersistentFlags().StringVar(&host, "cygnus-host", getKubeHost(kubeconfig), "Address of Cygnus (probably a Kubernetes master)")
+	cmd.PersistentFlags().StringVarP(&kubenamespace, "kube-namespace", "n", "kube-system", "Kubernetes namespace for Cygnus")
+	cmd.PersistentFlags().StringVar(&kubecontext, "kube-context", "", "Name of the kubeconfig context to use")
+
 	cmd.SetHelpTemplate(t.HelpTemplate)
 	cmd.SetUsageTemplate(t.UsageTemplate)
+
+	viper.BindPFlag("cygnus-host", cmd.PersistentFlags().Lookup("cygnus-host"))
+	viper.BindPFlag("cygnus-connection-timeout", cmd.PersistentFlags().Lookup("cygnus-connection-timeout"))
+	viper.BindPFlag("kubeconfig", cmd.PersistentFlags().Lookup("kubeconfig"))
+	viper.BindPFlag("kube-context", cmd.PersistentFlags().Lookup("kube-context"))
+	viper.BindPFlag("kube-namespace", cmd.PersistentFlags().Lookup("kube-namespace"))
 
 	return cmd
 }
 
 func runInit(cmd *cobra.Command, args []string) {
-	fmt.Println("info", "")
-	fmt.Println("info", "Now installing server component(s) into kube cluster")
-	fmt.Println("info", "")
+	h := viper.GetString("cygnus-host")
+	fmt.Println("")
+	fmt.Println("Cygnus (the Lyra server components) has been installed into a Kubernetes cluster at " + h + ".")
+	fmt.Println("")
+	fmt.Println("For more information on using Lyra with Cygnus, see https://github.com/lyraproj/docs/cygnus.md")
+}
+
+func getKubeHost(kubeconfig string) string {
+	// https://github.com/kubernetes/client-go/blob/master/examples/create-update-delete-deployment/main.go
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+	if err != nil {
+		panic(err)
+	}
+	if config.Host != "" {
+		return config.Host
+	}
+	return ""
 }
--- go.mod
+++ go.mod
@@ -1,19 +1,22 @@
 module github.com/lyraproj/lyra
 
 require (
+	github.com/BurntSushi/toml v0.3.1 // indirect
+	github.com/Netflix/go-expect v0.0.0-20180928190340-9d1f4485533b // indirect
 	github.com/aws/aws-sdk-go v1.16.20
 	github.com/boltdb/bolt v1.3.1
 	github.com/briandowns/spinner v0.0.0-20181029155426-195c31b675a7
+	github.com/cespare/trie v0.0.0-20150610204604-3fe1a95cbba9 // indirect
 	github.com/davecgh/go-spew v1.1.1
 	github.com/fatih/color v1.7.0 // indirect
-	github.com/go-logfmt/logfmt v0.4.0 // indirect
 	github.com/go-logr/logr v0.1.0
-	github.com/golang/mock v1.2.0 // indirect
 	github.com/hashicorp/go-hclog v0.0.0-20190109152822-4783caec6f2e
 	github.com/hashicorp/go-plugin v0.0.0-20181212150838-f444068e8f5a
 	github.com/hashicorp/yamux v0.0.0-20181012175058-2f1d1f20f75d // indirect
+	github.com/hinshun/vt10x v0.0.0-20180809195222-d55458df857c // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/kballard/go-shellquote v0.0.0-20180428030007-95032a82bc51 // indirect
+	github.com/kr/pretty v0.1.0 // indirect
 	github.com/leonelquinteros/gotext v1.4.0
 	github.com/lyraproj/hiera v0.0.0-20190114153236-e6de2935d4de
 	github.com/lyraproj/issue v0.0.0-20181208172701-8d203563a8dc
@@ -29,24 +32,22 @@ require (
 	github.com/pborman/uuid v1.2.0 // indirect
 	github.com/pkg/errors v0.8.1 // indirect
 	github.com/prometheus/client_model v0.0.0-20190115171406-56726106282f // indirect
-	github.com/sirupsen/logrus v1.3.0 // indirect
 	github.com/spf13/cobra v0.0.3
 	github.com/spf13/viper v1.3.1
 	github.com/stretchr/testify v1.3.0
 	github.com/toqueteos/trie v0.0.0-20150530104557-56fed4a05683 // indirect
 	golang.org/x/exp v0.0.0-20190104205336-ae74f88a12a8 // indirect
-	golang.org/x/lint v0.0.0-20181217174547-8f45f776aaf1 // indirect
 	golang.org/x/oauth2 v0.0.0-20190115181402-5dab4167f31c // indirect
 	golang.org/x/sys v0.0.0-20190116161447-11f53e031339 // indirect
-	golang.org/x/tools v0.0.0-20190116231616-b258f6da2383 // indirect
+	gonum.org/v1/netlib v0.0.0-20190119082159-9be13e02fd56 // indirect
 	google.golang.org/genproto v0.0.0-20190111180523-db91494dd46c // indirect
 	gopkg.in/AlecAivazis/survey.v1 v1.8.0
 	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 	gopkg.in/src-d/enry.v1 v1.6.7
 	gopkg.in/toqueteos/substring.v1 v1.0.2 // indirect
-	honnef.co/go/tools v0.0.0-20190109154334-5bcec433c8ea // indirect
 	k8s.io/apiextensions-apiserver v0.0.0-20190116054503-cf30b7cf64c2 // indirect
 	k8s.io/apimachinery v0.0.0-20190111195121-fa6ddc151d63 // indirect
+	k8s.io/client-go v10.0.0+incompatible
 	k8s.io/kube-openapi v0.0.0-20190115222348-ced9eb3070a5 // indirect
 	sigs.k8s.io/controller-runtime v0.1.9
 )
